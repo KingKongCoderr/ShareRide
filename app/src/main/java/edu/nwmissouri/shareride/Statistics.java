0d7adffc2114cf88e6e1c27bb915de8b2bf0ae48
Tankasala <S525120@mail.nwmissouri.edu>
2016-04-21 22:34:49 -0500

    Solved overlapping legend

--- app/src/main/java/edu/nwmissouri/shareride/Statistics.java
+++ app/src/main/java/edu/nwmissouri/shareride/Statistics.java
@@ -3,6 +3,7 @@ package edu.nwmissouri.shareride;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Display;
 import android.widget.RelativeLayout;
 import android.widget.RelativeLayout.LayoutParams;
@@ -23,10 +24,10 @@ import java.util.Iterator;
 import java.util.Map;
 
 public class Statistics extends AppCompatActivity {
-private RelativeLayout mstatisticslayout;
+    private RelativeLayout mstatisticslayout;
     private PieChart mChart;
-    private int[] ride_offers={5,10,20,35,40};
-    private String[] cities={"maryville","st.joseph","jefferson city","st.louis","Kansas city"};
+    private int[] ride_offers = {5, 10, 20, 35, 40};
+    private String[] cities = {"maryville", "st.joseph", "jefferson city", "st.louis", "Kansas city"};
 
     Client kinveyClient;
 
@@ -34,33 +35,30 @@ private RelativeLayout mstatisticslayout;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
-        mstatisticslayout = (RelativeLayout)findViewById(R.id.statisticslayout);
-        mChart=new PieChart(this);
+        mstatisticslayout = (RelativeLayout) findViewById(R.id.statisticslayout);
+        mChart = new PieChart(this);
         mstatisticslayout.addView(mChart);
         mstatisticslayout.setBackgroundColor(Color.LTGRAY);
 
         kinveyClient = new Client.Builder("kid_ZJCDL-Jpy-", "7ba9e5e0015849b790845e669ab87992", this.getApplicationContext()).build();
         Display screenDisplay = getWindowManager().getDefaultDisplay();
-        int width=screenDisplay.getWidth(),height=screenDisplay.getHeight();
-        LayoutParams layoutParams=new LayoutParams( width, height);
-        //layoutParams.setMargins(int left, int top, int right, int bottom);
+        int width = screenDisplay.getWidth() - 100, height = screenDisplay.getHeight() - 100;
+        LayoutParams layoutParams = new LayoutParams(width, height);
         mChart.setLayoutParams(layoutParams);
-       // mChart.setMinimumHeight();
-     //   mChart.setMinimumWidth(750);
         mChart.setDescription("Ride offers from cities");
         mChart.setDrawHoleEnabled(true);
         mChart.setHoleRadius(7);
         mChart.setTransparentCircleRadius(10);
         mChart.setRotationAngle(0);
         mChart.setRotationEnabled(true);
-
         addpieData();
+        Legend l = mChart.getLegend();
+        l.setPosition(Legend.LegendPosition.ABOVE_CHART_LEFT);
+        l.setXEntrySpace(5);
+        l.setTextSize(10f);
+        l.setYEntrySpace(5);
+        l.setWordWrapEnabled(true);
 
-        Legend l=mChart.getLegend();
-        l.setPosition(Legend.LegendPosition.BELOW_CHART_LEFT);
-        l.setXEntrySpace(1);
-        l.setTextSize(6f);
-        l.setYEntrySpace(1);
     }
 
     private void addpieData() {
@@ -72,57 +70,59 @@ private RelativeLayout mstatisticslayout;
                 ArrayList<String> states = new ArrayList<String>();
                 String[] cities;
                 int[] stateCount;
-                Map<String ,Integer> citiRides = new HashMap<String, Integer>();
-                for(Ride ride:rides){
-                    String[] bit = ride.getRouteFrom().split(",");
-                    String state = bit[bit.length-2];
-                    if(!states.contains(state)){
+                Map<String, Integer> citiRides = new HashMap<String, Integer>();
+                for (Ride ride : rides) {
+                    String[] bit = ride.getRouteFrom().split("[, ]+");
+
+                    Log.d("route",""+bit[bit.length-3]);
+                    String state = bit[bit.length - 3].substring(0,2);
+                    if (!states.contains(state)) {
                         states.add(state);
-                        citiRides.put(state,1);
-                    }else{
-                        citiRides.put(state, citiRides.get(state)+1);
+                        citiRides.put(state, 1);
+                    } else {
+                        citiRides.put(state, citiRides.get(state) + 1);
                     }
                 }
 
                 cities = new String[citiRides.size()];
                 stateCount = new int[citiRides.size()];
                 Iterator it = citiRides.entrySet().iterator();
-                int count= 0;
+                int count = 0;
                 while (it.hasNext()) {
-                    Map.Entry pair = (Map.Entry)it.next();
+                    Map.Entry pair = (Map.Entry) it.next();
                     cities[count] = (String) pair.getKey();
                     stateCount[count++] = (int) pair.getValue();
                     it.remove(); // avoids a ConcurrentModificationException
                 }
 
 
-                ArrayList<Entry> offervals=new ArrayList<Entry>();
+                ArrayList<Entry> offervals = new ArrayList<Entry>();
 
-                for(int i=0;i<stateCount.length;i++){
-                    offervals.add(new Entry(stateCount[i],i));
+                for (int i = 0; i < stateCount.length; i++) {
+                    offervals.add(new Entry(stateCount[i], i));
                 }
 
-                ArrayList<String> citievals=new ArrayList<String>();
-                for(int j=0;j<cities.length;j++){
+                ArrayList<String> citievals = new ArrayList<String>();
+                for (int j = 0; j < cities.length; j++) {
                     citievals.add(cities[j]);
                 }
-                PieDataSet dataSet = new PieDataSet(offervals,"ride offers");
+                PieDataSet dataSet = new PieDataSet(offervals, "  Ride Offers");
                 dataSet.setHighlightEnabled(true);
                 dataSet.setSliceSpace(3);
                 dataSet.setSelectionShift(5);
 
-                ArrayList<Integer> colors=new ArrayList<Integer>();
-                for(int c: ColorTemplate.JOYFUL_COLORS)
+                ArrayList<Integer> colors = new ArrayList<Integer>();
+                for (int c : ColorTemplate.JOYFUL_COLORS)
                     colors.add(c);
-                for(int c: ColorTemplate.VORDIPLOM_COLORS)
+                for (int c : ColorTemplate.VORDIPLOM_COLORS)
                     colors.add(c);
-                for(int c: ColorTemplate.COLORFUL_COLORS)
+                for (int c : ColorTemplate.COLORFUL_COLORS)
                     colors.add(c);
 
                 dataSet.setColors(colors);
                 dataSet.setValueTextColor(Color.BLACK);
 
-                PieData data=new PieData(citievals,dataSet);
+                PieData data = new PieData(citievals, dataSet);
                 data.setValueTextSize(11f);
                 data.setValueTextColor(Color.BLACK);
 
